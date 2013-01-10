833b9fd52ae93aec3df523bcf5821437232bd582
rasokan <rashok55@gmail.com>
2013-01-10 21:40:34 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
 	public static void main(String a[])
 	{
-		System.out.println("Hello world Github Program");
+		System.out.println("Hello world Github Program for First Pull Request");
 	}
 }
